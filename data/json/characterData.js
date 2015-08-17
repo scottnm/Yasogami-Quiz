058fa71aca5be3beddb8d1339318b0e7567500f6
Scott Munro <scottmunro@utexas.edu>
2015-08-16 22:35:12 -0500

    added the image urls

--- data/json/characterData.js
+++ data/json/characterData.js
@@ -1,52 +1,52 @@
 var charData = {
 	"characters": [
 		{
-			"name": "Teddy",
+			"name": "Teddie",
 			"description": "Give up on school. Your talents are more suited to stocking groceries and making bear puns. You will lead a very empty life.",
 			"percentageThreshold": "12",
-			"image": ""
+			"image": "images/teddie.png"
 		},
 		{
 			"name": "Rise",
 			"description": "I'm sure people told you, you were very talented growing up right? Education is not one of your talents. I'm sure you could write one hell of a pop single though.",
 			"percentageThreshold": "25",
-			"image": ""
+			"image": "images/rise.png"
 		},
 		{
 			"name": "Kanji",
 			"description": "School isn't your thing, but let's face it... you are way too masculine for school. I'm sure you could beat the hell out of me so who am I to criticize you?",
 			"percentageThreshold": "37",
-			"image": ""
+			"image": "images/kanji.png"
 		},
 		{
 			"name": "Yosuke",
 			"description": "You are just average. Enjoy your average life.",
 			"percentageThreshold": "50",
-			"image": ""
+			"image": "images/yosuke.png"
 		},
 		{
 			"name": "Chie",
 			"description": "You score poorly on the Math & Science sections, but score well on your humanities exams, especially those involving martial arts and military histories.",
 			"percentageThreshold": "62",
-			"image": ""
+			"image": "images/chie.png"
 		},
 		{
 			"name": "Yukiko",
 			"description": "You study well and it shows. If only you weren't as boring as tap water.",
 			"percentageThreshold": "75",
-			"image": ""
+			"image": "images/yukiko.png"
 		},
 		{
 			"name": "Yu",
 			"description": "Surprise surprise, the main character scored well. I'm sure you looked up an online walkthrough to answer these questions correctly. Enjoy your score Gary Stu.",
 			"percentageThreshold": "87",
-			"image": ""
+			"image": "images/yu.png"
 		},
 		{
 			"name": "Naoto",
 			"description": "Outstanding scores. I have nothing more to say than outstanding. You really are a prodigy. Congratulations!",
 			"percentageThreshold": "100",
-			"image": ""
+			"image": "images/naoto.png"
 		}
 	]
 };
